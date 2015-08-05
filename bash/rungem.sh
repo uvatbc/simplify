08d3ab3a18cefd87f7a23eeb08add3fa61c36360
Yuvraaj Kelkar <yuvraaj@gmail.com>
2015-08-05 11:12:17 -0700

    documentation for rungem.sh

--- bash/rungem.sh
+++ bash/rungem.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Usage: rungem.sh <gem to run>
+
 GEM_INSTALL_LOCATION=/var/lib/gems/2.1.0/gems/
 FINALPATH="$PATH"
 
+#Find every bin directory in the GEM_INSTALL_LOCATION and add it to $PATH
 while read line ; do
     if [ "$line" == "$GEM_INSTALL_LOCATION" ] ; then
         continue
@@ -11,7 +14,9 @@ while read line ; do
     if [ -d "$line/bin" ] ; then
         FINALPATH="$line/bin:$FINALPATH"
     fi
+# This is required to allow the updated value of FINALPATH to be used outside this loop.
+# See: http://mywiki.wooledge.org/BashFAQ/024
 done < <(find $GEM_INSTALL_LOCATION  -maxdepth 1)
 
-echo PATH=$FINALPATH $*
+#echo PATH=$FINALPATH $*
 PATH=$FINALPATH $*
